c0b8ca82ad8644b9b3d3d1f914358fcc480e96bf
Ajaj <ajaj121ahmad@gmail.com>
2026-04-09 23:39:36 +0530

    Pushing Movie Booking LLD

---
+++ Movie Booking/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+
+enum BookingStatus
+{ 
+    Pending, 
+    Failed,
+    Confirmed,
+    Cancelled
+}
+enum SeatStatus
+{
+    Locked,
+    Booked,
+    Available
+}
+enum SeatType
+{
+    Silver, Gold, Platinum
+}
+enum NotificationType
+{
+    Email, SMS
+}
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("AJAJ");
+    }
+
+}
