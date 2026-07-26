6a501ef1d2343bebc1604898d4e2d7ddc9797d12
Ajaj <ajaj121ahmad@gmail.com>
2026-07-26 20:57:39 +0530

    Corrected and formatted the methods

--- Library Management System/LibraryManagement/Program.cs
+++ Library Management System/LibraryManagement/Program.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Diagnostics.Contracts;
-
-public enum BookStatus
+﻿enum BookStatus
 {
     Available,
     Borrowed,
     Damaged
 }
 
-public enum BorrowStatus
+enum BorrowStatus
 {
     Active,
     Returned,
     Overdue
 }
+
 public class User
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+
     public User(int id, string name, string email)
     {
         this.Id = id;
@@ -30,26 +29,20 @@ public class User
 public class BookCopy
 {
     public int Id { get; set; }
-    public BookStatus Status { get; set; }
+    public BookStatus Status { get; private set; }
     public Book ParentBook { get; set; }
+
     public BookCopy(int id, Book parentBook)
     {
         this.Id = id;
         this.ParentBook = parentBook;
         this.Status = BookStatus.Available;
     }
-    public bool IsAvailable()
-    {
-        return Status == BookStatus.Available;
-    }
-    public void MarkBorrowed()
-    {
-        Status = BookStatus.Borrowed;
-    }
-    public void MarkDamaged()
-    {
-        Status = BookStatus.Damaged;
-    }
+
+    public bool IsAvailable() => Status == BookStatus.Available;
+    public void MarkBorrowed() => Status = BookStatus.Borrowed;
+    public void MarkReturned() => Status = BookStatus.Available;
+    public void MarkDamaged() => Status = BookStatus.Damaged;
 }
 
 public class Book
@@ -59,6 +52,7 @@ public class Book
     public string Author { get; set; }
     public List<BookCopy> Copies { get; set; }
     public Queue<User> Waitlist { get; set; }
+
     public Book(int id, string title, string author)
     {
         this.Id = id;
@@ -68,12 +62,8 @@ public class Book
         this.Waitlist = new Queue<User>();
     }
 
-    public bool IsAvailable()
-    {
-        return Copies.Any(copy => copy.IsAvailable());
-    }
+    public bool IsAvailable() => Copies.Any(copy => copy.IsAvailable());
+
     public BookCopy GetAvailableCopy()
-    {
-        return Copies.FirstOrDefault(copy => copy.IsAvailable());
-    }
+        => Copies.FirstOrDefault(copy => copy.IsAvailable());
 }
